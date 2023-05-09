60afa9f2dae9374ce5915a6c260dce4a7345efd0
sameersinha3 <78034576+sameersinha3@users.noreply.github.com>
2023-05-09 12:43:34 -0400

    Update vendor.sql

--- vendor.sql
+++ vendor.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Creato il: Mag 02, 2023 alle 16:37
--- Versione del server: 10.4.27-MariaDB
--- Versione PHP: 8.2.0
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -16,16 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `vendor`
---
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `application`
---
 
 CREATE TABLE `application` (
   `id` int(11) NOT NULL,
@@ -40,19 +23,13 @@ CREATE TABLE `application` (
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dump dei dati per la tabella `application`
---
+
 
 INSERT INTO `application` (`id`, `id_user`, `name`, `description`, `category`, `link`, `instagram`, `facebook`, `twitter`, `active`) VALUES
 (14, 6, 'My First App', 'This is a description, modified!', 'School', 'https://www.example.com', 'https://www.my_instagram_page.com', '', 'https://twitter.com', 1),
 (16, 6, 'App created from ADMIN', 'Description of this app...', 'Research', 'https://text.com', '', '', '', 1);
 
--- --------------------------------------------------------
 
---
--- Struttura della tabella `user`
---
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
@@ -65,43 +42,23 @@ CREATE TABLE `user` (
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dump dei dati per la tabella `user`
---
+
 
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `psw`, `phone`, `role`, `active`) VALUES
 (2, 'Admin', 'Admin', 'admin@gmail.com', '123456', '123456789', 1, 1),
 (6, 'Marc', 'User', 'user@gmail.com', '123456', '333456789', 0, 1);
 
---
--- Indici per le tabelle scaricate
---
 
---
--- Indici per le tabelle `application`
---
 ALTER TABLE `application`
   ADD PRIMARY KEY (`id`);
 
---
--- Indici per le tabelle `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT per le tabelle scaricate
---
 
---
--- AUTO_INCREMENT per la tabella `application`
---
 ALTER TABLE `application`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
---
--- AUTO_INCREMENT per la tabella `user`
---
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
